52031edc3f39d13dfed05dfffa11dd3440dd70b9
Aydeniztr <88689203+Aydeniztr@users.noreply.github.com>
2022-02-26 11:50:53 +0300

    syntax errors fixed

--- message-parser.py
+++ message-parser.py
@@ -7,45 +7,74 @@ banner = '''
   |___|_|__|_   _     _  _  _    ,   __, 
   |   |\|  |  |/ \_  / |/ |/ |  / \_/  | 
   |   |/|_/|_/|__/     |  |  |_/ \/ \_/|/
-             /|                       /| 
-             \|                       \| 
+	     /|                       /| 
+	     \|                       \| 
 '''
 
 msg = '''
-authow:Aydeniztr
+author:Aydeniztr
 '''
 
 usg = '''
 usage: 
+
 python3 message-parser.py <url>
+
+costum_argvs:
+
+[('--help'),('--version'),('--about')]
 
 '''
 
-def get_mag(link)
+about= '''
+this script parses http-messages
+and prints the items
+'''
+
+none = ''
+
+cat = r'''
+    |\__/,|   (`\
+  _.|o o  |_   ) )
+-(((---(((--------
+'''
+
+#edit here to change the mascot
+
+mascot = none
+
+ver = '''
+version:1.0.4
+'''
+def get_msg(link):
+
+	print('sending request to the' + argv[1])
 
 	meta = urlopen(link)
 
+	print('parsing http-return-message\n')
+	
 	u = meta.info()
-
+	
 	print(u)
 	
 if len(argv) <= 1:
 	
-	print(banner+msg+usg)
+	print(banner+msg+about+usg+ver+mascot)
 	
-elif argv[1] == '--version'
+elif argv[1] == '--version':
 
-	print('ver:'+ version)
+	print(banner+version+mascot)
 	exit()
 
 
-elif argv[1] == '--about'
-	print(msg)
+elif argv[1] == '--about':
+	print(banner+msg+about+usg+ver+mascot)
 
-elif argv[1] == '--help'
-	print(usg)
+elif argv[1] == '--help':
+	print(banner+msg+about+usg+ver+mascot)
 else:
 	
 	print(banner)
 	get_msg(argv[1])
-	print('sending request to the' + sys.argv[1])
+	
